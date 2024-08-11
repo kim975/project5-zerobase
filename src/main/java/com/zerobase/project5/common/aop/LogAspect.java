d513bd3876aa658dde28bd35c5ea59f685699c58
kim <scilo200@gmail.com>
2024-08-12 00:55:32 +0900

    Aop Request Logging 추가

---
+++ src/main/java/com/zerobase/project5/common/aop/LogAspect.java
@@ -0,0 +1,58 @@
+package com.zerobase.project5.common.aop;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+@Aspect
+@Slf4j
+@Component
+public class LogAspect {
+
+    @Pointcut("execution(* com.zerobase.project5.domain.*.controller.*Controller.*(..))")
+    public void controller() {
+
+    }
+
+    @Around("controller()")
+    public Object loggingRequest(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+
+        String controllerName = joinPoint.getSignature().getDeclaringType().getName();
+        String methodName = joinPoint.getSignature().getName();
+        Map<String, Object> params = new HashMap<>();
+
+        try {
+            String decodedURI = URLDecoder.decode(request.getRequestURI(), "UTF-8");
+
+            params.put("controller", controllerName);
+            params.put("method", methodName);
+//            params.put("params", getParams(request));
+            params.put("log_time", System.currentTimeMillis());
+            params.put("request_uri", decodedURI);
+            params.put("http_method", request.getMethod());
+        } catch (Exception e) {
+            log.error("LoggerAspect error", e);
+        }
+
+        log.info("[{}] {}", params.get("http_method"), params.get("request_uri"));
+        log.info("method: {}.{}", params.get("controller") ,params.get("method"));
+        log.info("params: {}", params.get("params"));
+
+        Object proceed = joinPoint.proceed();
+
+        return proceed;
+    }
+
+}
